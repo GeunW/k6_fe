6826eddbd1b07683fb72f2eaae25503810af547d
GeunW <trampsoul32@gmail.com>
2024-03-08 16:18:13 +0900

    240308

--- 10/10.js
+++ 10/10.js
@@ -1,72 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
     //DOM 생성후에 img, input, button 가져오기  
-    const bcolor = document.querySelectorAll("span");
     const bt = document.querySelector("button");
+    const numD = document.querySelector("#numD");
 
-    const numInput = document.getElementById("num1");
-    
-    const msg = document.querySelector("#msg");
+    let nums = [];
 
-    //시작시 포커스
-    bcolor.style.background = "none";
+    bt.addEventListener("click", () => {
+        nums.length=0;
 
-    //랜덤수 생성 변수
-    let n;
-    let flag = false;
-
-    //버튼의 클릭이벤트 달기
-    bt.addEventListener("click", (e) => {
-        e.preventDefault();
-        //text박스 보이기
-        bcolor.style.background = "yellow";
-
-        //랜덤수 생성
-        // if (flag === false) {
-        if (!flag) {
-            n = Math.floor(Math.random() * 46) + 1; //1~100까지
-            console.log("n=", n);
-            flag = true;
-
-            //입력초기화
-            numInput.style.display = "inline";
-            bt.innerHTML = "확인";
-            numInput.value = "";
-            numInput.focus();
-            upDownImg.setAttribute("src", "./img/what.png");
+        //랜덤 수 만들기 : 1~45 , 7개, 중복 x
+        while (nums.length < 7) {
+            let n = Math.floor(Math.random() * 45) + 1;
+            if(!nums.includes(n))nums.push(n);
         }
 
+        // let tags = '';
+        // nums.map((v,i) => {
+        //     tags = tags+`<span class="sp${parseInt(v/10)}">${v}</span> `;
+        //     if(i===5){
+        //         tags=tags+`<span>+</span>`
+        //     }
+        // });
+        // numD.innerHTML = tags
 
-
-        //숫자비교
-        msg.innerHTML = "";
-        if (numInput.value > 100 || numInput.value < 0) {
-            msg.innerHTML = "<span>1~100까지 숫자를 입력하세요.<span>"
-        }
-
-        if (n === parseInt(numInput.value)) { //타입 비교 or if (n == numInput.value) 
-            upDownImg.setAttribute("src", "./img/good.png");
-            numInput.style.display = "none";
-            bt.innerHTML = "재시작";
-            flag = false;
-
-        }
-        else if (n > numInput.value) {
-            upDownImg.setAttribute("src", "./img/up.png");
-        }
-        else {
-            upDownImg.setAttribute("src", "./img/down.png");
-        }
-
-        //input 박스 내용가져오기
-        if (numInput.value == '') {
-            // alert("숫자를 입력하세요.") ;
-            msg.innerHTML = "<span>숫자를 입력하세요.<span>";
-            numInput.focus();
-            return;
-        }
-
-
+        
+        nums = nums.map((v, i) =>
+            `<span class="sp${parseInt(v / 10)}">${v}</span> `
+        );
+        nums.splice(6, 0, '<span>+</span>');
+      
+        numD.innerHTML = nums.toString().replaceAll(',','');
+        console.log(nums)
     });
-
-
 });
